ae9102e41b10910f01b97bb30bb637d75df06de8
forkan <for70600@gmail.com>
2026-03-02 11:29:25 +0300

    to do 1.2

--- 5-5-react-route-form/src/App.jsx
+++ 5-5-react-route-form/src/App.jsx
@@ -38,46 +38,6 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Registration from "./pages/Registration";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="app">
-        <nav className="navbar">
-          <div className="brand"> Student Portal</div>
-
-          <div className="links">
-            {/* Nav links */}
-            <NavLink to="/" end className="navlink">
-              Home
-            </NavLink>
-
-            <NavLink to="/about" className="navlink">
-              About
-            </NavLink>
-
-            <NavLink to="/registration" className="navlink">
-              Registration
-            </NavLink>
-          </div>
-        </nav>
-
-        <main className="container">
-          {/* Routes */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/registration" element={<Registration />} />
-            <Route path="*" element={<h2>404 — Not Found</h2>} />
-          </Routes>
-        </main>
-
-        <footer className="footer">
-          <span>© {new Date().getFullYear()} React Student Portal</span>
-        </footer>
-      </div>
-    </BrowserRouter>
-  );
-}
 // ===========================
 //
 // Goal: Add routing for three pages: Home, About, Registration
@@ -197,11 +157,28 @@ export default function App() {
         <div className="brand">🧑‍💻 Student Portal</div>
         <div className="links">
           {/*Nav links*/}
+          <NavLink to="/" end className="navlink">
+              Home
+            </NavLink>
+
+            <NavLink to="/about" className="navlink">
+              About
+            </NavLink>
+
+            <NavLink to="/registration" className="navlink">
+              Registration
+            </NavLink>
         </div>
       </nav>
 
       <main className="container">
         {/*Routes*/}
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="*" element={<h2>404 — Not Found</h2>} />
+          </Routes>
       </main>
 
       <footer className="footer">
